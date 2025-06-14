b0443d869e04d0db4b6846cd99cc98d9ce09521c
root <root@srv815567.hstgr.cloud>
2025-06-14 07:10:42 +0000

    update in production

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:5173')],
+    'allowed_origins' => [env('FRONTEND_URL', 'https://osintwork.com')],
 
     'allowed_origins_patterns' => [],
 
